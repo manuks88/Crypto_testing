5ca10aa7d0bd8b2de3070b514073f5b706f8b2ff
Manu KS <cse.manuks@gmail.com>
2017-06-02 16:52:53 +0530

    Script to generate data file.

---
+++ functional/generate_data_file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+rm -rf $(pwd)/data_file.txt
+for i in {1..60}
+{
+        size=$(( RANDOM % 1250))
+        if [ $size == "0" ]
+        then
+        {
+                size="1250"
+        }
+	fi
+	for (( j=1;j<=$size;j++ ))
+	{
+	        printf %s {0..9} >> $(pwd)/data_file.txt
+	        printf %s {a..z} >> $(pwd)/data_file.txt
+	        printf %s {A..Z} >> $(pwd)/data_file.txt
+	        echo -n '$!' >> $(pwd)/data_file.txt
+	}
+        printf "\n" >> $(pwd)/data_file.txt
+        echo $i
+}
